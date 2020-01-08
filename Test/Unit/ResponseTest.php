8abd6c9e6c37dac354587bb5145b0975cd66ab8c
Willian Keller <williank@ciandt.com>
2020-01-08 09:15:36 -0500

    Improve PHPdocs

--- Test/Unit/ResponseTest.php
+++ Test/Unit/ResponseTest.php
@@ -1,25 +1,14 @@
 <?php
 
-/**
- * Log Webapi: Module provides log in file for all transactions in Web API.
- * Copyright (C) Magestat
- *
- * This file included in Magestat/LogWebapi is licensed under OSL 3.0
- *
- * http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
- * Please see LICENSE.txt for the full text of the OSL 3.0 license
- */
-
 namespace Magestat\LogWebapi\Test\Unit;
 
 /**
  * Class ResponseTest
+ * Tests to the response data.
  */
 class ResponseTest extends \PHPUnit\Framework\TestCase
 {
     /**
-     * Response object.
-     *
      * @var Response
      */
     private $_response;
